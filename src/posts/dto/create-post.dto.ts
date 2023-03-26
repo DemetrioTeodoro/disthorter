ae95bb57cf943dd0cb6aafad4f92f79ae8231f44
Demetrio <demetrio.teodoro@gmail.com>
2023-03-25 23:47:17 -0300

    Adicionado class-validator, atualizadas dependencias

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,8 +1,25 @@
-import { User } from "src/user/entities/user.entity";
+import { ArrayMinSize, IsArray, IsMongoId, IsNotEmpty, IsNotEmptyObject, IsString, MaxLength, MinLength, ValidateNested } from "class-validator";
+import { CreateUserDto } from "src/user/dto/create-user.dto";
 
 export class CreatePostDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4)
     type: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
     message: string;
+
+    @IsString()
+    @IsNotEmpty()
     imagePath: string;
-    users: [User];
+
+    @IsArray()
+    @IsNotEmpty()
+    @ArrayMinSize(1)
+    @ValidateNested({ each: true })
+    users: CreateUserDto[];
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,22 @@
+import { IsDate, IsDateString, IsEmail, IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator";
+
 export class CreateUserDto {
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
   password: string;
+
+  @IsDate()
+  @IsNotEmpty()
   birthDate: Date;
 }
