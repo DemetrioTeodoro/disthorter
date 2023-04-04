4730c49603eda3ef78be48a7f09484f57973ee85
Demetrio <demetrio.teodoro@gmail.com>
2023-04-03 23:35:50 -0300

    Ajuste para modelo user mapear mais de um post

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { HydratedDocument } from 'mongoose';
+import { Post } from 'src/posts/entities/post.entity';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema({ versionKey: false })
+@Schema({ versionKey: false, timestamps: true })
 export class User {
   @Prop({ required: true })
   name: string;
@@ -16,6 +18,9 @@ export class User {
 
   @Prop({ required: true })
   birthDate: Date;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }] })
+  posts: Post[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
