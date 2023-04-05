8e7347f178a9d3360440d50a61c2a04c6f9edb19
Demetrio <demetrio.teodoro@gmail.com>
2023-04-04 23:05:05 -0300

    Adicionado address na entidade user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { HydratedDocument } from 'mongoose';
+import { Address } from 'src/address/entities/address.entity';
 import { Post } from 'src/posts/entities/post.entity';
 
 export type UserDocument = HydratedDocument<User>;
@@ -21,6 +22,9 @@ export class User {
 
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }] })
   posts: Post[];
+  
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Address' }] })
+  addresses: Address[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
