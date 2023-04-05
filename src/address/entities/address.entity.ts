7040f55d112b92a78ff85fe3f6e0fcf02725c0da
Demetrio <demetrio.teodoro@gmail.com>
2023-04-04 23:00:33 -0300

    Criada entiade address

---
+++ src/address/entities/address.entity.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { User } from 'src/user/entities/user.entity';
+
+export type AddressDocument = HydratedDocument<Address>;
+
+@Schema({ versionKey: false, timestamps: true })
+export class Address {
+    @Prop({ required: true })
+    country: string;
+
+    @Prop({ required: true })
+    state: string;
+
+    @Prop({ required: true })
+    city: string;
+
+    @Prop({ required: true })
+    street: number;
+
+    @Prop({ required: true })
+    number: string;
+
+    @Prop({ required: true })
+    complement: string;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    user: User;
+}
+
+export const AddressSchema = SchemaFactory.createForClass(Address);
