331edb522fdf7f3f3c67cd0e6145dee2a0c9a939
Demetrio <demetrio.teodoro@gmail.com>
2023-04-07 18:51:55 -0300

    feat: Adicionado import para model address

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AddressService } from './service/address.service';
 import { AddressController } from './controller/address.controller';
+import { Address, AddressSchema } from './entities/address.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Address.name, schema: AddressSchema }]),
+  ],
   controllers: [AddressController],
   providers: [AddressService]
 })
