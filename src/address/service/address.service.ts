f860763db052f76f4a0d0fa7f08b16c2459a2ea1
Demetrio <demetrio.teodoro@gmail.com>
2023-04-07 19:02:50 -0300

    feat: Adicionado types e retorno de dados

--- src/address/service/address.service.ts
+++ src/address/service/address.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { CreateAddressDto } from '../dto/create-address.dto';
 import { UpdateAddressDto } from '../dto/update-address.dto';
+import { Address, AddressDocument } from '../entities/address.entity';
 
 @Injectable()
 export class AddressService {
-  create(createAddressDto: CreateAddressDto) {
-    return 'This action adds a new address';
+
+  constructor(@InjectModel(Address.name) private addressModel: Model<AddressDocument>) { }
+
+  async create(createAddressDto: CreateAddressDto): Promise<CreateAddressDto> {
+    try {
+      return await new this.addressModel(createAddressDto).save();
+    } catch (error) {
+      console.error(`Erro ao salvar endereço: ${error.message}`);
+      throw new Error('Ocorreu um erro ao salvar endereço.');
+    }
   }
 
-  findAll() {
-    return `This action returns all address`;
+  async findAll(): Promise<CreateAddressDto[]> {
+    try {
+      return await this.addressModel.find();
+    } catch (error) {
+      console.error(`Erro ao buscar endereços: ${error.message}`);
+      throw new Error('Ocorreu um erro ao buscar endereços.');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} address`;
+  async findOne(id: string): Promise<CreateAddressDto> {
+    try {
+      return await this.addressModel.findById(id);
+    } catch (error) {
+      console.error(`Erro ao buscar endereço: ${error.message}`);
+      throw new Error('Ocorreu um erro ao buscar endereço.');
+    }
   }
 
-  update(id: number, updateAddressDto: UpdateAddressDto) {
-    return `This action updates a #${id} address`;
+  async update(id: string, updateAddressDto: UpdateAddressDto): Promise<CreateAddressDto> {
+    try {
+      return await this.addressModel.findByIdAndUpdate({ _id: id, }, { $set: updateAddressDto, }, { new: true });
+    } catch (error) {
+      console.error(`Erro ao atualizar endereço: ${error.message}`);
+      throw new Error('Ocorreu um erro ao atualizar endereço.');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} address`;
+  async remove(id: string) {
+    try {
+      return await this.addressModel.deleteOne({ _id: id });
+    } catch (error) {
+      console.error(`Erro ao remover endereço: ${error.message}`);
+      throw new Error('Ocorreu um erro ao remover endereço.');
+    }
   }
 }
