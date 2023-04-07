21e6da82dd382a835c20ed3262a6548193544ae1
Demetrio <demetrio.teodoro@gmail.com>
2023-04-07 00:03:53 -0300

    fix: Ajuste de import do service address

--- src/address/controller/address.controller.ts
+++ src/address/controller/address.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AddressService } from '../address.service';
+import { AddressService } from '../service/address.service';
 import { CreateAddressDto } from '../dto/create-address.dto';
 import { UpdateAddressDto } from '../dto/update-address.dto';
 
